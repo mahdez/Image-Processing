eaee1600e445d97b234b590a1921af09426a1199
mahdez <mahdee.dark@gmail.com>
2018-03-08 16:50:53 +0700

    upload project

---
+++ BMP/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace BMP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnThreshold = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(333, 56);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(197, 159);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnThreshold
+            // 
+            this.btnThreshold.Location = new System.Drawing.Point(311, 333);
+            this.btnThreshold.Name = "btnThreshold";
+            this.btnThreshold.Size = new System.Drawing.Size(75, 23);
+            this.btnThreshold.TabIndex = 2;
+            this.btnThreshold.Text = "Threshold";
+            this.btnThreshold.UseVisualStyleBackColor = true;
+            this.btnThreshold.Click += new System.EventHandler(this.btnThreshold_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(92, 333);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open file";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "90 องศา";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Location = new System.Drawing.Point(473, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "270 องศา";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(554, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Contrast";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(632, 56);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 242);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(619, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Gamma";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(632, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(636, 333);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Histrogram";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 390);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnThreshold);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnThreshold;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ BMP/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMP
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap f_image = null;
+        public Bitmap image;
+        int m = 0;
+        int sin90 = 1, sin270 = -1;
+        int cos90 = 0, cos270 = 0;
+        double gamma = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Filter = "bitmap (*.bmp)|*.bmp";
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (f_image != null) f_image.Dispose();
+                    f_image = (Bitmap)Bitmap.FromFile(openFileDialog1.FileName, false);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Can not open file", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            image = new Bitmap(f_image.Width, f_image.Height);
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    //  num_pic.Add(C_gray);
+                    m = m + C_gray;
+
+                    image.SetPixel(j, i, Color.FromArgb(C_gray, C_gray, C_gray));
+
+                }
+            }
+            m = m / (f_image.Width * f_image.Height);
+            pictureBox1.Image = f_image;
+            pictureBox2.Image = image;
+        }
+
+        private void btnThreshold_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    
+                    if (C_gray > m)
+                    {
+                        C_gray = 255;
+                    }
+                    else
+                    {
+                        C_gray = 0;
+                    }
+                    image.SetPixel(j, i, Color.FromArgb(C_gray, C_gray, C_gray));
+
+                }
+            }
+            pictureBox2.Image = image;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int Cont = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    
+                    int s = 0;
+                    if (Cont > 150)
+                    {
+                        s = (int)((0.523 * Cont) + 121.635);
+                    }
+                    else if (Cont >=100)
+                    {
+                        s = (int)((3 * Cont) - 250);
+                    }
+                    else
+                    {
+                        s = (int)(0.5 * Cont);
+                    }
+                    image.SetPixel(j, i, Color.FromArgb(s, s, s));
+                }
+            }
+            
+            pictureBox2.Image = image;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int c = 1;
+            gamma = (double)trackBar1.Value / 10.0;
+            label2.Text = gamma.ToString();
+            
+
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(i, j);
+                    int img = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    // img = (img / 10) ;
+
+                    // img.ToString()
+                    int s = c * (int)Math.Pow(img,gamma);
+                    s = 255 - s;
+                    if (s > 255)
+                    {
+                        s = s / 10;
+                        s = 255 - s;
+                    }
+                    if (s<0)
+                    {
+                        s = (s / 10) * -1;
+                        
+                    }
+                    image.SetPixel(i, j, Color.FromArgb(s, s, s));
+                    //textBox1.Text = s.ToString();
+                }
+            }
+            pictureBox2.Image = image;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            //array No. of pixel
+            int[] Nop = new int[256];
+            for (int i = 0; i < 256; i++)
+            {
+                Nop[i] = 0;
+            }
+
+            //array No. of frequency
+            int[] Nof = new int[256];
+
+            //array No. of frequency / Total number pixel
+            float[] Nopf = new float[256];
+
+            //array No.of possible
+            int[] NoP = new int[256];
+
+            //count No. of pixel
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    Nop[C_gray]++;
+
+                }
+
+            }
+
+            //count No. of frequency
+            Nop[0] = Nof[0];
+            for (int i = 1; i < 256; i++)
+            {
+                Nof[i] = Nof[i - 1] + Nop[i];
+            }
+
+            //count No. of frequency / Total number pixel
+            int NM = f_image.Height * f_image.Width;
+            for (int i = 0; i < 256; i++)
+            {
+                Nopf[i] = (float)Nof[i] / NM;
+            }
+
+            //count No. of possible
+
+            for (int i = 0; i < 256; i++)
+            {
+                NoP[i] = (int)(Nopf[i] * 255);
+            }
+
+            //output
+            int ss;
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(j, i);
+                    int C_gray = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    for (int k = 0; k < 256; k++)
+                    {
+                        if (C_gray == k)
+                        {
+                            ss = NoP[k];
+                            image.SetPixel(j, i, Color.FromArgb(ss, ss, ss));
+                            break;
+                        }
+
+                    }
+                }
+            }
+            pictureBox2.Image = image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(i, j);
+                    int img = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    int imgX = 128+((i-128) * cos90) + ((j-128) * sin90);
+                    int imgY = 128+((i-128) * sin90) + ((j-128) * cos90);
+                    image.SetPixel(imgX, imgY, Color.FromArgb(img, img, img));
+
+                }
+            }
+            pictureBox2.Image = image;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+            for (int i = 0; i < f_image.Height; i++)
+            {
+                for (int j = 0; j < f_image.Width; j++)
+                {
+                    Color PixelColor = f_image.GetPixel(i, j);
+                    int img = (int)(PixelColor.R + PixelColor.G + PixelColor.B) / 3;
+                    int imgX = 127 + ((i - 128) * 0) + ((j - 128) * -1);
+                    int imgY = 127 + ((i - 128) * -1) + ((j - 128) * 0);
+                    
+                    image.SetPixel(imgX, imgY, Color.FromArgb(img, img, img));
+
+                }
+            }
+            pictureBox2.Image = image;
+        }
+    } 
+}
